5b37ec7b1143e655790869dd69fc838f3c701492
mahendra7041 <chavdamahendra247@gmail.com>
2026-03-04 19:53:29 +0530

    feat: database class added

---
+++ packages/axe-api/src/Database.ts
@@ -0,0 +1,25 @@
+import knex from "knex";
+import { AppLoader } from "./AppLoader";
+import { LogService } from "./Services";
+import { attachPaginate } from "knex-paginate";
+
+export default class Database {
+  static create(): knex.Knex {
+    const appLoader = AppLoader.getInstance();
+    const database = knex(appLoader.map.config.database);
+    attachPaginate();
+
+    const client = appLoader.map.config.database.client;
+    const connection = appLoader.map.config.database.connection as {
+      db?: string;
+      filename?: string;
+    };
+    const { db, filename } = connection;
+
+    LogService.debug(
+      `Created a knex connection instance: [${client}:${db || filename}]`,
+    );
+
+    return database;
+  }
+}
